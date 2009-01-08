288dc14834b5af1498d85168a025956e5e24f142
mihasya <m@mihasya.com>
2009-01-07 18:01:10 -0800

    added uniqueness validation to add point form

--- forms.py
+++ forms.py
@@ -1,6 +1,21 @@
 from django import forms
+from models import Point
+from google.appengine.ext import db
+from django.utils.translation import ugettext_lazy as _
+from google.appengine.api import users
 
 class AddPointForm(forms.Form):
     name = forms.CharField(max_length=255)
     short_name = forms.CharField(max_length=50)
     
+    def clean_short_name(self):
+        #verify that the short_name is unique
+        value = self.cleaned_data['short_name']
+        q = db.Query(Point)
+        q.filter('name =', value)
+        q.filter('user =', users.get_current_user())
+        if (q.get()):
+            raise forms.ValidationError(_("A point with that \
+                        id exists already"))
+        else:
+            return value
