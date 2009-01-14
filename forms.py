0f55616a345b5debdf4d06ed93c4e0f8e0a665e3
mihasya <m@mihasya.com>
2009-01-13 17:47:43 -0800

    made the form generate a slug for name based on description

--- forms.py
+++ forms.py
@@ -3,21 +3,22 @@ from models import Bmark
 from google.appengine.ext import db
 from django.utils.translation import ugettext_lazy as _
 from google.appengine.api import users
+import re
 
 class AddBmarkForm(forms.Form):
-    name = forms.CharField(max_length=50)
+    name = forms.CharField(max_length=50, required=False)
     description = forms.CharField(max_length=255)
-    
+
     def clean_name(self):
         #verify that the short_name is unique
-        value = self.cleaned_data['name']
-        if (value.find(' ')!=-1):
-            raise forms.ValidationError(_("Name shouldn't contain spaces"))
+        desc = self.cleaned_data['description']
+        name = re.sub('[^\w|^\-| ]', '', desc)
+        name = name.replace(' ', '_')
         q = db.Query(Bmark)
-        q.filter('name =', value)
+        q.filter('name =', name)
         q.filter('user =', users.get_current_user())
         if (q.get()):
             raise forms.ValidationError(_("A bookmark with that \
                         name exists already"))
         else:
-            return value
+            return name
